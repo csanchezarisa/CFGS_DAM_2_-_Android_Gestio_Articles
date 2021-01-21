f36b31c80fa6dce8bf9d6ebabd91378933850440
ulkiobal <ulkiobal@users.noreply.github.com>
2021-01-21 17:34:55 +0100

    Menú personalizado en MovementsHistoryActivity

--- app/src/main/java/com/example/gestioarticles/activities/stockmanage/MovementsHistoryActivity.java
+++ app/src/main/java/com/example/gestioarticles/activities/stockmanage/MovementsHistoryActivity.java
@@ -1,18 +1,76 @@
 package com.example.gestioarticles.activities.stockmanage;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.gestioarticles.R;
 
 public class MovementsHistoryActivity extends AppCompatActivity {
 
+    /* .: 1. VARIABLES GLOBALS :. */
+    /** Variable que emmagatzema el id de l'article que s'està gestionant.
+     * -1 si no hi ha cap seleccionat */
     private long idArticle;
 
+
+    /* .: 2. CREACIÓ DE L'ACTIVITY :. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movements_history);
+
+        // Es modifica la ActionBar per mostrar el botò per retornar i canviar el títol
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(getString(R.string.activity_movements_history_title));
+    }
+
+
+    /* .: 3. MENÚ PERSONALITZAT :. */
+    /** Menú personalitzat amb icones d'acció */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_movements_history_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finalitzarActivity();
+                return true;
+            case R.id.menu_btn_search_article_history:
+                return true;
+            case R.id.menu_btn_history_order:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /* .: 4. ACCIONS PER FINALITZAR L'ACTIVITY :. */
+    /** Finalitza l'activity, marcant com a ResultCode un OK */
+    private void finalitzarActivity() {
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    // Listener que controla l'acció del botó "Endarrera" del telèfon
+    @Override
+    public void onBackPressed() {
+        finalitzarActivity();
     }
 }
