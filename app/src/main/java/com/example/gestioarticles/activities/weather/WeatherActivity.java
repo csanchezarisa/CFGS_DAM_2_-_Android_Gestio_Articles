8bc4654d5a98d9bab3482b557be27b7bf503c37b
csanchezarisa <61799628+csanchezarisa@users.noreply.github.com>
2021-02-17 17:23:13 +0100

    WeatherActivity creada

---
+++ app/src/main/java/com/example/gestioarticles/activities/weather/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.example.gestioarticles.activities.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gestioarticles.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
